45bb83823ab34ad2be9564642995780c7127fed8
crackman2 <pombenenge@googlemail.com>
2024-11-20 15:42:28 +0100

    reasonably functional

--- src/cmd_handler.nim
+++ src/cmd_handler.nim
@@ -1,9 +1,9 @@
-  import dimscord, os, osproc, asyncdispatch, strutils
+import dimscord, os, osproc, asyncdispatch, strutils, options, streams
 
 
 var
   last_channel_id:string
-
+  watch_command_active:bool = false
 
 type
   TCMDHandler* = object
@@ -16,6 +16,33 @@ proc sendMsg(self:TCMDHandler, m:string, channel_id:string):Future[bool] {.async
   discard await self.discord.api.sendMessage(channel_id, m)
   return true
 
+proc watchCommand(self:TCMDHandler, command:string, channel_id:string):Future[bool] {.async.} =
+  echo "running command: " & command
+  #var command_out = execCmdEx("./" & command & " &> tmp.log &")
+  var process_handle = startProcess("./" & command)
+  var output_handle = outputStream(process_handle)
+  var msg = await self.discord.api.sendMessage(channel_id, "temp")
+  while watch_command_active:
+    #var (output, exitcode) = execCmdEx("tail -n 25 tmp.log")
+    var output = ""
+    for i in 0..10:
+      var line = readLine(output_handle)
+      output = output & "\n" & line
+
+    discard self.discord.api.editMessage(channel_id, msg.id, embeds = @[Embed(
+        title: some "Watch Command",
+        description: some "output:\n" & output,
+        color: some 0x7789ec
+        )]
+    )
+  
+    await sleepAsync(5000)
+  
+  close(process_handle)
+  discard self.sendMsg("watch beendet", channel_id)
+  return true
+
+
 proc handleMessage*(self:TCMDHandler, m:Message):Future[bool] {.async.} =
   # keep channel id for future use
   last_channel_id = m.channel_id
@@ -24,7 +51,23 @@ proc handleMessage*(self:TCMDHandler, m:Message):Future[bool] {.async.} =
 
   case mtokens[0]:
     of "!ping":
-      discard self.sendMsg("Da haben sie mich falsch angepingt, sie müssen das anders machen!", m.channel_id)
+      discard self.sendMsg("Da haben Sie mich falsch angepingt, Sie hätten das anders machen müssen!", m.channel_id)
+    of "!pwd":
+      var curdir = getCurrentDir()
+      discard self.sendMsg("Derzeitig befinde ich mich in: " & curdir, m.channel_id)
+    of "!watch":
+      var combine_tokens  = mtokens[1..^1].join(" ")
+      discard self.sendMsg("Nein. Also ich sage Ihnen nur, was sie schon wieder angerichtet haben: '" & combine_tokens & "'", m.channel_id)
+      watch_command_active = true      
+      discard watchCommand(self, combine_tokens, m.channel_id)
+    of "!stop":
+      if watch_command_active:
+        discard self.sendMsg("In Ordnung. Es reicht.", m.channel_id)
+        watch_command_active = false
+      else:
+        discard self.sendMsg("Was meinen Sie damit? Ich mache doch gar nichts! Anzeige ist raus.", m.channel_id)
     else:
       echo "cmd: invalid command, ignoring."
   return true
+
+
---
+++ src/testcmd.nim
@@ -0,0 +1,7 @@
+import os
+
+var tmp = 0
+while true:
+    echo("num: " & $(tmp))
+    tmp = tmp + 1
+    sleep(1000)
