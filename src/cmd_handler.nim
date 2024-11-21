f4666ab88cf822916f92402336dcd4b7b043588a
crackman2 <pombenenge@googlemail.com>
2024-11-21 15:01:14 +0100

    cleanup input

--- src/cmd_handler.nim
+++ src/cmd_handler.nim
@@ -26,14 +26,21 @@ proc sendMsg(self:TCMDHandler, m:string, channel_id:string):Future[bool] {.async
   discard await self.discord.api.sendMessage(channel_id, m)
   return true
 
+proc readLineAsync(s:Stream):Future[string] {.async.} =
+  return readLine(s)
+
 proc watchCommand(self:TCMDHandler, command:string, channel_id:string):Future[bool] {.async.} =
   if not (command in self.command_whitelist):
-    discard self.sendMsg("Wer glauben Sie eigentlich wer Sie sind? Das dürfen Sie nicht und Sie werden von meinem Anwalt hören.", channel_id)
+    #discard self.sendMsg("Wer glauben Sie eigentlich wer Sie sind? Das dürfen Sie nicht und Sie werden von meinem Anwalt hören.", channel_id)
     echo "ERROR: command not in whitelist. stopping !watch"
+    discard await self.sendMsg(spruchPicker("watcherror"), channel_id)
     watch_command_active = false
+    raise newException(ValueError, "invalid command")
     return false
 
   echo "running command: " & command
+  discard await self.sendMsg(spruchPicker("watch") & " : '" & command & "'", channel_id)
+
   var
     process_handle = startProcess("./" & command)
     output_handle = outputStream(process_handle)
@@ -47,7 +54,7 @@ proc watchCommand(self:TCMDHandler, command:string, channel_id:string):Future[bo
     var log_line = ""
 
     try:
-      log_line = readLine(output_handle)
+      log_line = await readLineAsync(output_handle)
       echo "kw_watch '" & command& "': " & log_line
     except:
       echo "ERROR: couldn't read stdout. Exiting Loop"
@@ -92,7 +99,8 @@ proc watchCommand(self:TCMDHandler, command:string, channel_id:string):Future[bo
       #echo "printed successfully :)" 
   
   close(process_handle)
-  discard self.sendMsg("watch beendet", channel_id)
+  #discard self.sendMsg("watch beendet", channel_id)
+  echo "watch ended"
   return true
 
 
@@ -103,26 +111,55 @@ proc handleMessage*(self:TCMDHandler, m:Message):Future[bool] {.async.} =
   let mtokens = m.content.split(" ")
 
   case mtokens[0]:
+    of "!help":
+      var edit_msg = await self.discord.api.sendMessage(m.channel_id, embeds = @[Embed(
+        title: some "Befehle",
+        description: some "!ping\n!pwd\n!path\n!watch\n!stop",
+        color: some 0x7789ec
+        )]
+      )
     of "!ping":
       discard self.sendMsg(spruchPicker("ping"),m.channel_id)
     of "!pwd":
       var curdir = getCurrentDir()
-      discard self.sendMsg("Derzeitig befinde ich mich in: " & curdir, m.channel_id)
+      discard self.sendMsg(spruchPicker("pwd") & curdir, m.channel_id)
     of "!path":
-      var combine_tokens  = mtokens[1..^1].join(" ")
-      setCurrentDir(combine_tokens)
-      discard self.sendMsg("Ich bin jetzt in: " & getCurrentDir(), m.channel_id)
+      var
+        combine_tokens = ""
+        errors = false
+      if mtokens.len > 1:
+        combine_tokens = mtokens[1..^1].join(" ")
+        if combine_tokens.strip != "":
+          try:
+            setCurrentDir(combine_tokens)
+            discard self.sendMsg(spruchPicker("path") & getCurrentDir(), m.channel_id)
+          except:
+            errors = true
+        else:
+          errors = true
+      else:
+        errors = true
+      if errors: discard self.sendMsg(spruchPicker("patherror"), m.channel_id)
     of "!watch":
-      var combine_tokens  = mtokens[1..^1].join(" ")
-      discard self.sendMsg( spruchPicker("befehl") & " : '" & combine_tokens & "'", m.channel_id)
-      watch_command_active = true      
-      discard watchCommand(self, combine_tokens, m.channel_id)
+      var
+        combine_tokens = ""
+        errors = false
+      if mtokens.len > 1:
+        combine_tokens  = mtokens[1..^1].join(" ")
+        if combine_tokens.strip != "":
+          watch_command_active = true      
+          discard watchCommand(self, combine_tokens, m.channel_id)
+        else:
+          errors = true
+      else:
+        errors = true
+      if errors: discard self.sendMsg(spruchPicker("watcherror"), m.channel_id)
     of "!stop":
       if watch_command_active:
-        discard self.sendMsg("In Ordnung. Es reicht.", m.channel_id)
+        discard self.sendMsg(spruchPicker("stop"), m.channel_id)
         watch_command_active = false
       else:
-        discard self.sendMsg("Was meinen Sie damit? Ich mache doch gar nichts! Anzeige ist raus.", m.channel_id)
+        discard self.sendMsg(spruchPicker("stoperror"), m.channel_id)
     else:
       echo "cmd: invalid command, ignoring."
   return true
--- src/spruch.nim
+++ src/spruch.nim
@@ -4,22 +4,28 @@ var
   randomize_init = false
   
   last_index_ping = -1
-  last_index_commandexecuted = -1
+  last_index_watch = -1
+  last_index_watch_error = -1
+  last_index_stop_watch = -1
+  last_index_stop_watch_error = -1
+  last_index_pwd = -1
+  last_index_path = -1
+  last_index_path_error = -1
 
   sprueche_ping = @[
     "Da haben Sie mich falsch angepingt, Sie hätten das anders machen müssen!",
     "Sie halten sich wohl für besonders lustig, was?",
-    "Ich erzähle Ihnen jetzt mal was: Das Pingen hat war früher viel schneller und einfacher als heutzutage!",
+    "Ich erzähle Ihnen jetzt mal was: Das Pingen war früher viel schneller und einfacher als heutzutage!",
     "Wenn man weiß was man tut, muss man praktisch nie jemanden anpingen. Aber das erfordert eben Können.",
     "Ich weiß nicht ob du mich anpingen _kannst_. _Kannst_ du?",
-    "So ein Verhalten hätte zu meiner Zeit nie gegeben. Was heutzutage alles erlaubt ist... Irrsinn!",
+    "So ein Verhalten hätte es zu meiner Zeit nie gegeben. Was heutzutage alles erlaubt ist... Irrsinn!",
     "So manch einer behauptet ja, dass das Anpingen zum normalen Ablauf dazugehöre. Dies ist ein Trugschluss und das kann ich nicht verstehen und dann muss sowas auch verboten werden!"
   ]
 
-  sprueche_befehl_ausgefuehrt = @[
-    "Nein. Also ich sage Ihnen nur, was sie schon wieder angerichtet haben",
+  sprueche_watch = @[
+    "Nein. Also ich sage Ihnen nur, was Sie schon wieder angerichtet haben",
     "Folgendes habe ich meinem Anwalt mitgeteilt",
-    "Glauben Sie, sie kommen ungeschoren davon, wenn Sie",
+    "Glauben Sie, Sie kommen ungeschoren davon, wenn Sie",
     "In meinem Vorgarten liegen noch Reste von Ihrem",
     "Sie können sich auch nun gar nicht anständig benehmen mit Ihrem",
     "Für sowas gehören Sie in den Bau",
@@ -27,26 +33,73 @@ var
     "Den Schaden müssen Sie mir bezahlen! Es ist nicht normal zu"
   ]
 
+  sprueche_watch_error = @[
+    "Also das habe ich jetzt überhaupt nicht verstanden. Ich glaube Sie wissen selbst nicht was Sie wollen.",
+    "Für solche Scherzereien habe ich keine Zeit. Nerven Sie jemand anderes!",
+    "Sie halten sich wohl für den größten Komiker.",
+    "Ich schlage Ihnen gleich mit meinen Birkenstocks das Grinsen aus.",
+    "Sie wissen schon, dass Sie sich mit solchen Spielerein strafbar machen, oder?",
+    "Netter Versuch, aber ich bin Ihnen mal wieder mit Leichtigkeit überlegen.",
+    "Zu meiner Zeit gab es solche Witze gar nicht. Da hat man noch von Natur aus gelacht."
+  ]
+
+  sprueche_stop_watch = @[
+    "In Ordnung. Es reicht.",
+    "Ich hoffe, dass es das letzte Mal gewesen ist.",
+    "Ein Dankeschön ist angebracht. Denken Sie mal drüber nach.",
+    "Schon vorbei? Ich habe mir bereits gedacht, dass Sie das wieder alles falsch machen.",
+    "Schämen Sie sich eigentlich gar nicht? Oder ist das gar Absicht?",
+    "Wer so arbeitet, muss sich gar nicht über den Zustand wundern.",
+    "Diese ganzen Computer-Fritze machen immer alles nur schlimmer!"
+  ]
+
+  sprueche_stop_watch_error = @[
+    "Was meinen Sie damit? Ich mache doch gar nichts! Anzeige ist raus.",
+    "Was reden Sie denn da für einen Unfug?",
+    "Leute wie Sie sollte man wegsperren. Hoffnungslos. Und dann auch noch mit Jogginghose.",
+    "Na? Wieder alles kaputt gemacht? Hier ist eine Liste von Menschen, die das überrascht:",
+    "Das hat mir fast die Krawatte zerknittert. Passen Sie gefälligst auf, wenn Sie mit solchen Befehlen herumwedeln!",
+    "Das mag vielleicht normal sein, wo Sie herkommen. Aber das hier ist DEUTSCHLAND."
+  ]
+
+  sprueche_pwd = @[
+    "Ich befinde mich derzeitig in: ",
+    "Meine Bestellungen bei Tamonda werden geliefert nach: ",
+    "Bei mir haben sie neulich das Fenster mit Eiern beworfen in: ",
+    "Die Bratwurst schmeckt am besten bei: ",
+    "Mit meinen Birkenstocks kann ich sogar hierhin spazieren: ",
+    "Hier? Meine Adresse bekommen Sie nicht. Nichtmal diese hier: ",
+    "Ich hoffe doch sehr, dass Sie mich mal besuchen kommen. Eintritt 5€: ",
+    "Meine Steuererklärung mach im Schlaf bei: ",
+    "Die Medikamente erinnern mich daran wo ich wohne: "
+  ]
+
+  sprueche_path = @[
+    "Umziehen muss jeder mal: ",
+    "In den Urlaub fahren war ja so einfach. Ich bin in: ",
+    "Schon wieder ein Ausflug. Spannend hier in: ",
+    "Ach hätt ich doch nur meinen Fotoappart hierhin mitgenommen: ",
+    "So weit weg von zuhause. Ungewohnt in: ",
+    "Ich hoffe doch sehr, dass die Leute hier wissen was Bratwurst und Bier ist in: ",
+    "Meine Birkenstocks haben mich hierher geführt: ",
+    "Ohne meinen Pullunder hätte ich mich nicht hierher getraut: ",
+    "Der Mercedes fährt mich mit Stil nach: "
+  ]
+
+  sprueche_path_error = @[
+    "Ich habe sogar in der App auf meinem Mobiltelefonapparat geschaut, den Ort gibt es so nicht!",
+    "Von diesem Land habe ich noch nichts gehört. Muss wohl eines dieser \"-istan\"-Länder sein.",
+    "Zu meiner Zeit konnte jeder perfekt Karte lesen. Das können Sie ja scheinbar nicht.",
+    "Nein. Also den Ort gibt es gar nicht. Hören Sie auf so einen Unsinn zu verbreiten!",
+    "Früher hätte man den Ort viel leichter gefunden. Aber die Zeiten sind vorbei. Schlechte Wegbeschreibung.",
+    "Gehen Sie doch selbst dort hin, wenn es diesen Ort gibt! Verarschen kann ich mich auch selber.",
+    "Da kommen Sie wohl her, oder? Wie die ganzen anderen nutzlosen Jogginghosenträger."
+  ]
+
 proc spruchRnd():void =
   if not randomize_init:
     randomize()
     randomize_init = true
-
-proc spruchPing*():string =
-  spruchRnd()
-  var index = last_index_ping
-  while index == last_index_ping:
-    index = rand(high(sprueche_ping))
-  last_index_ping = index
-  return sprueche_ping[index]
-
-proc spruchCommandExecuted*():string =
-  spruchRnd()
-  var index = last_index_commandexecuted
-  while index == last_index_commandexecuted:
-    index = rand(high(sprueche_befehl_ausgefuehrt))
-  last_index_commandexecuted = index
-  return sprueche_befehl_ausgefuehrt[index]
 
 proc spruchPicker*(topic:string):string =
   var
@@ -57,9 +110,27 @@ proc spruchPicker*(topic:string):string =
   of "ping":
     selected = sprueche_ping
     last_index = addr last_index_ping
-  of "befehl":
-    selected = sprueche_befehl_ausgefuehrt
-    last_index = addr last_index_commandexecuted
+  of "watch":
+    selected = sprueche_watch
+    last_index = addr last_index_watch
+  of "watcherror":
+    selected = sprueche_watch_error
+    last_index = addr last_index_watch_error
+  of "stop":
+    selected = sprueche_stop_watch
+    last_index = addr last_index_stop_watch
+  of "stoperror":
+    selected = sprueche_stop_watch_error
+    last_index = addr last_index_stop_watch_error
+  of "pwd":
+    selected = sprueche_pwd
+    last_index = addr last_index_pwd
+  of "path":
+    selected = sprueche_path
+    last_index = addr last_index_path
+  of "patherror":
+    selected = sprueche_path_error
+    last_index = addr last_index_path_error
   else:
     return "FEHLER: Da weiß ich bald gar nicht was ich dazu sagen soll. Furchtbar sowas."
   spruchRnd()
